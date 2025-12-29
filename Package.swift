9e28f72e5674bd7515363016a06341f95a816040
ludovic grimbert <ludovicgrimbert@orange.fr>
2025-12-29 06:44:14 +0100

    Clean

--- Package.swift
+++ Package.swift
@@ -18,7 +18,7 @@ let package = Package(
 
     ],
     dependencies: [
-        .package(url: "https://github.com/firebase/firebase-ios-sdk.git", from: "10.0.0")
+        .package(url: "https://github.com/firebase/firebase-ios-sdk.git", from: "12.7.0")
     ],
     targets: [
         // Targets are the basic building blocks of a package, defining a module or a test suite.
